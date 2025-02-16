656dadbf501274dba4697d16cf7461da45b0af45
mdiener87 <118212366+mdiener87@users.noreply.github.com>
2025-02-16 16:48:00 -0700

    testing app.config color

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full py-6 shadow-md">
+  <header class="w-full py-6 shadow-md bg-primary">
     <div class="container mx-auto text-center">
       <h1 class="text-4xl font-bold">Welcome to Diener.tech</h1>
       <p class="text-lg mt-2">
