c76a91d22d563502f49dd0fc330e8a620590d810
mdiener87 <118212366+mdiener87@users.noreply.github.com>
2025-02-16 15:14:45 -0700

    add a transition for <NuxtPage>

--- app.vue
+++ app.vue
@@ -4,10 +4,24 @@
   >
     <Header />
     <main class="max-w-3xl mx-auto px-6 py-6 flex-grow w-full">
-      <NuxtPage />
+      <Transition name="fade" mode="out-in">
+        <NuxtPage />
+      </Transition>
     </main>
     <Footer />
   </div>
 </template>
 
 <script setup></script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
