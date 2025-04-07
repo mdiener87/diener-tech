63edde9938c2544b776a6449dc6df87c73bf3c88
mdiener87 <118212366+mdiener87@users.noreply.github.com>
2025-04-07 00:05:57 -0600

    just rasterize it lolllll
    lollllll

--- plugins/content-components.ts
+++ plugins/content-components.ts
@@ -1,22 +1,12 @@
 import BlogImage from "~/components/content/BlogImage.vue";
 import GithubProject from "~/components/content/GithubProject.vue";
-import BlogCollapsible from "~/components/content/BlogCollapsible.vue";
-import Collapsible from "~/components/content/Collapsible.vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (!nuxtApp.vueApp._context.components["BlogImage"]) {
     nuxtApp.vueApp.component("BlogImage", BlogImage);
   }
 
-if (!nuxtApp.vueApp._context.components["GithubProject"]) {
+  if (!nuxtApp.vueApp._context.components["GithubProject"]) {
     nuxtApp.vueApp.component("GithubProject", GithubProject);
-}
-
-if (!nuxtApp.vueApp._context.components["BlogCollapsible"]) {
-  nuxtApp.vueApp.component("BlogCollapsible", BlogCollapsible);
-}
-
-if (!nuxtApp.vueApp._context.components["Collapsible"]) {
-  nuxtApp.vueApp.component("Collapsible", Collapsible);
-}
+  }
 });
